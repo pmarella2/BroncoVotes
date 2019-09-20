a40b205b43971778b5d54b348f36bbd785bddd39
Praneeth Babu Marella <9484118+pmarella2@users.noreply.github.com>
2019-09-20 04:50:09 +0000

    Adding hex conversion of strings in the array

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = function(deployer) {
     deployer.deploy(Voting, 1499573503, 1, 5, 1234567890, 'Testing Phase Ballot', 0, '0x1e376f3b3d7afba2a8ca5ff288e7f5d9585fdae8', {
         gas: 2000000
     });
-    deployer.deploy(Registrar, ['gmail.com', 'yahoo.com', 'hotmail.com'], {
+    deployer.deploy(Registrar, ['gmail.com', 'yahoo.com', 'hotmail.com'].map(x => web3.utils.asciiToHex(x)), {
         gas: 800000
     });
     deployer.deploy(Creator, {
